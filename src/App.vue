8476942c2399573e9cff54d32a1c2b8bddcf1bb3
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2025-06-02 08:32:35 +0200

    created first component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <div id="app"></div>
+  <BaseCard>
+    <h2 style="color: white;">Titel 1</h2>
+    <p style="color: white;">Inhalt für die erste Karte.</p>
+  </BaseCard>
 </template>
 
 <script>
+import BaseCard from './components/BaseCard.vue';
+
 export default {
   name: 'App',
   components: {
-
+    BaseCard
   }
 }
 </script>
@@ -17,7 +22,9 @@ body {
 }
 
 #app {
+  padding: 100px;
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
+  box-sizing: border-box;
   min-height: 100vh;
   width: 100vw;
 }
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="baseCard">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseCard',
+};
+</script>
+
+<style scoped>
+.baseCard {
+  background-color: #011F35;
+  border-radius: 16px;
+  padding: 20px 32px;
+}
+</style>
